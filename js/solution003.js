ea2916bd483e1f65279cd876a291458a47d1d076
Ryan-Perez <perezryanm@gmail.com>
2022-05-18 22:23:32 -0700

    Reveresed String

---
+++ js/solution003.js
@@ -0,0 +1,12 @@
+// Question
+/* 
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+*/
+
+//Answer
+function solution(str){
+    return str.split("").reverse().join("")
+  }
