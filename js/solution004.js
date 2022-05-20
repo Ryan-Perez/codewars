ac5b5c7f3c8e3f7dd2f8becc32513a1661cae702
Ryan-Perez <perezryanm@gmail.com>
2022-05-20 15:13:31 -0700

    What is between

---
+++ js/solution004.js
@@ -0,0 +1,20 @@
+//Question
+// Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+// For example:
+
+// a = 1
+// b = 4
+// --> [1, 2, 3, 4]
+
+
+//Answer
+
+function between(a, b) {
+    let list = []
+    for (var i = a; i <= b; i++){
+      list.push(i)
+      }
+     return list
+  }
+  
