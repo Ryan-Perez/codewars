a1707cf2b30493d9fb566ef74757ab4f652d33d4
ryan perez <perezryanm@gmail.com>
2022-06-17 10:29:26 -0700

    Total Goals

---
+++ js/toUpperCase.js
@@ -0,0 +1,10 @@
+/* Instructions
+Write a function which converts the input string to uppercase.
+
+*/
+
+//Answer
+function correct(string)
+{
+return string.replace(/5/g,'S').replace(/0/g,'O').replace(/1/g,'I');
+}
