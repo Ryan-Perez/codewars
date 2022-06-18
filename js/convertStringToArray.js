1f2822f769730b34ffe0e3bf581f4b174af9679a
ryan perez <perezryanm@gmail.com>
2022-06-18 10:58:19 -0700

    String to array

---
+++ js/convertStringToArray.js
@@ -0,0 +1,10 @@
+/*Instructions
+Write a function to split a string and convert it into an array of words.
+*/
+
+//Answer
+function stringToArray(str){
+
+    return str.trim().split(" ");
+   
+   }
