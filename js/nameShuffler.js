fc0830b84f718ef6c30a6e8d7095f8704f879ed4
ryan perez <perezryanm@gmail.com>
2022-06-21 11:30:44 -0700

    drinking age

---
+++ js/nameShuffler.js
@@ -0,0 +1,10 @@
+/* Instructions
+Write a function that returns a string in which firstname is swapped with last name.
+*/
+
+//Answer
+function nameShuffler(str){
+    return str.split(" ").reverse().join(" ")
+  }
+
+  
