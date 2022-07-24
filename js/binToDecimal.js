e46c4d0a65fe6099f57c375e96f7e918109c3121
Ryan-Perez <perezryanm@gmail.com>
2022-07-24 12:34:04 -0700

    bin to decimal

---
+++ js/binToDecimal.js
@@ -0,0 +1,8 @@
+/*Instructions
+Complete the function which converts a binary number (given as a string) to a decimal number.
+*/
+
+//Answer
+function binToDec(bin){
+    return parseInt(bin, 2)
+  }
